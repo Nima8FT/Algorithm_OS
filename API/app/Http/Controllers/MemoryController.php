37fde7061c84d1a3cf3a77f8f2b1a21334ab10b2
Nima <nima_8ak@yahoo.com>
2024-07-20 09:13:59 +0330

    finish worstfit algorithm in memory allocation

--- API/app/Http/Controllers/MemoryController.php
+++ API/app/Http/Controllers/MemoryController.php
@@ -94,4 +94,51 @@ class MemoryController extends Controller
             );
         }
     }
+
+    public function worst_fit(Request $request)
+    {
+        if ($request->input('Algorithm') == "WorstFit") {
+            $block_array = explode(' ', $request->get("Block"));
+            $process_array = explode(' ', $request->get("Process"));
+
+            $j = 0;
+            $processes = [];
+            $is_process_complete = array_fill(0, count($process_array), false);
+            $chart = [];
+
+            while ($j < count($block_array)) {
+                $shortest_process = 0;
+                $process_num = -1;
+
+                for ($i = 0; $i < count($process_array); $i++) {
+                    if ($block_array[$j] <= $process_array[$i] && !$is_process_complete[$i] && $process_array[$i] >= $shortest_process) {
+                        $shortest_process = $process_array[$i];
+                        $process_num = $i;
+                    }
+                }
+
+                if ($process_num != -1) {
+                    $processes[] = [
+                        'process' => $j + 1,
+                        'process_size' => $process_array[$process_num],
+                        'block_size' => $block_array[$j],
+                        'left_over' => $process_array[$process_num] -= $block_array[$j],
+                    ];
+                    $chart[] = [
+                        'process' => $j + 1,
+                        'chart' => $process_array,
+                    ];
+                    $is_process_complete[$process_num] = true;
+                }
+                $j++;
+            }
+
+            return response()->json(
+                [
+                    'process' => $processes,
+                    'chart' => $chart,
+                ],
+            );
+        }
+    }
 }
--- API/routes/api.php
+++ API/routes/api.php
@@ -23,3 +23,4 @@ Route::post('/priority/preemptive', [CpuController::class, 'priority_preemptive'
 //memory allocation controller routes
 Route::post('/bestfit', [MemoryController::class,'best_fit']);
 Route::post('/firstfit', [MemoryController::class,'first_fit']);
+Route::post('/worstfit', [MemoryController::class,'worst_fit']);
